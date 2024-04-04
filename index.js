887e3a38431dff60d5406ecf74510b3c53ef8992
Stephen Ochieng <materiamystic@gmail.com>
2024-04-04 13:58:12 +0300

    Adds fetch POST functionality

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+  const body = document.querySelector("body");
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const dataDiv = document.createElement("div");
+      dataDiv.innerHTML = `
+        <p> <b>ID</b>: <span>${data.id}</span></p>
+        `;
+      body.append(dataDiv);
+    })
+    .catch((error) => {
+      const errorDiv = document.createElement("div");
+      errorDiv.innerHTML = `
+        <p> <b>Error message</b>: <span>${error.message}</span></p>
+        `;
+      body.append(errorDiv);
+    });
+}
+
+//test code
+//submitData("Gary", "gary@example3.com");
